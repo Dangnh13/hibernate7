55bdc5dc747ed53401a61c2dd5b157b8ae314105
dangnh <dang.nh.aprotrain@gmail.com>
2025-02-22 23:51:06 +0700

    Tải nhiều entity sử dụng API byMultipleIds

--- src/test/java/test_l1/TestInheritance.java
+++ src/test/java/test_l1/TestInheritance.java
@@ -15,6 +15,8 @@ import util.HibernateUtil;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestInheritance {
 
     @BeforeAll
@@ -60,9 +62,17 @@ public class TestInheritance {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
         var debitAccount = new entities.inheritance.singleTable.DebitAccount();
-        debitAccount.setOwner("debit Account");
+        debitAccount.setOwner("debit Account 0 ");
         debitAccount.setOverdraftFee(new BigDecimal(1000));
         session.persist(debitAccount);
+        var debitAccount1 = new entities.inheritance.singleTable.DebitAccount();
+        debitAccount1.setOwner("debit Account 1");
+        debitAccount1.setOverdraftFee(new BigDecimal(1111));
+        session.persist(debitAccount1);
+        var debitAccount2 = new entities.inheritance.singleTable.DebitAccount();
+        debitAccount2.setOwner("debit Account 2");
+        debitAccount2.setOverdraftFee(new BigDecimal(2000));
+        session.persist(debitAccount2);
 
         var creditAccount = new entities.inheritance.singleTable.CreditAccount();
         creditAccount.setOwner("credit");
@@ -76,6 +86,22 @@ public class TestInheritance {
         var foundCre = session.find(entities.inheritance.singleTable.CreditAccount.class, creditAccount.getId());
         System.out.println(foundDebit.toString());
         System.out.println(foundCre.toString());
+
+        System.out.println("-------Tải nhiều entity sử dụng API byMultipleIds của Hibernate ---");
+        // binding parameter (1:ARRAY) <- [[1, 2]]
+        var debits = session
+                .byMultipleIds(entities.inheritance.singleTable.DebitAccount.class)
+                .multiLoad(1L, 2L);
+
+        assertEquals(2, debits.size());
+
+        // binding parameter (1:ARRAY) <- [[3]]
+        var sameDebits = session
+                .byMultipleIds(entities.inheritance.singleTable.DebitAccount.class)
+                .enableSessionCheck(true)
+                .multiLoad(1L, 2L, 3L);
+
+        assertEquals(debits, sameDebits);
         session.close();
     }
 
